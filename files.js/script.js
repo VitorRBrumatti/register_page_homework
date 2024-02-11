8f9d7daa779180a1be207f6e334a739db47cbf14
VitorRBrumatti <vitorrufino084@gmail.com>
2024-02-11 18:19:36 -0300

    mudança de html e tentativa de validação de e-mail

--- files.js/script.js
+++ files.js/script.js
@@ -1,15 +1,58 @@
 let telefone = document.getElementById("telefone");
 let nome = document.getElementById("name");
-let email = document.getElementById("email");
-let termos = document.getElementById("checkbox");
-let errorE = document.getElementById("textErrorE");
+let email = document.getElementById("email").value;
+let termos = document.getElementById("termos");
+let senha = document.getElementById("senha")
+let errorText = document.getElementById("errorText");
+let errorEmail = document.getElementById("errorE");
+let errorSenha =  document.getElementById("errorSenha");
+let errorTelefone = document.getElementById("errorTelefone");
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 function enviar() {
-    if (!email.value) {
-        errorE.style.color = "orange";
-        errorE.innerText = "Insira um email para avançar" ; 
-        return
+    if (!nome.value) { 
+        errorText.style.color = "white";
+        errorText.innerText = "Preencha o campo para avançar" ;
     }
-    errorE.style.color = "lime"
-    errorE.innerText = "Campo preenchido corretamente!";
+    else {
+        errorText.style.color = "lime"
+        errorText.innerText = "Campo preenchido corretamente!";
+    }
+
+    if (!telefone.value) {
+        errorTelefone.style.color = "white";
+        errorTelefone.innerText = "Insira seu Telefone para avançar" ; 
+    
+    }
+    else {
+        errorTelefone.style.color = "lime";
+        errorTelefone.innerText = "Campo preenchido corretamente!" ; 
+    }
+
+    if (!email) {
+        errorE.style.color = "white";
+        errorE.innerText = "Preencha com o email para avançar" ;
+    
+    }
+    else if(email.match(pattern)) {
+        errorE.style.color = "orange";
+        errorE.innerText = "Insira um E-mail válido" ;
+     }
+    else {     
+        errorE.style.color = "lime";
+        errorE.innerText = "Campo preenchido corretamente!" ;
+    }
+
+    if (!senha.value) {
+        errorSenha.style.color = "white";
+        errorSenha.innerText = "Insira a sua nova senha para avançar" ;
+    }
+    else if (senha.value.length < 8) {
+        errorSenha.style.color = "orange";
+        errorSenha.innerText = "Insira no mínimo 8 caracteres" ;
+    }
+    else {
+        errorSenha.style.color = "lime";
+        errorSenha.innerText = "Campo Preenchido corretamente" ;
+    }
 }
