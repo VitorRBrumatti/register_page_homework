d1cb7bc88971d7b76e5c1402c699ba0b2cbc8f44
VitorRBrumatti <vitorrufino084@gmail.com>
2024-02-11 21:51:12 -0300

    validate finished

--- files.js/script.js
+++ files.js/script.js
@@ -1,6 +1,8 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 let telefone = document.getElementById("telefone");
 let nome = document.getElementById("name");
-let email = document.getElementById("email").value;
 let termos = document.getElementById("termos");
 let senha = document.getElementById("senha")
 let errorText = document.getElementById("errorText");
@@ -10,53 +12,74 @@ let errorTelefone = document.getElementById("errorTelefone");
 const validade = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 function enviar() {
+    let botaoDisable = document.getElementById("button")
+    let email = document.getElementById("email").value;
+// validação do nome
     if (!nome.value) { 
-        errorText.style.color = "white";
+        errorText.style.color = "red";
         errorText.innerText = "Preencha o campo para avançar" ;
+        botaoDisable.disabled = true;
     }
     else {
         errorText.style.color = "lime"
         errorText.innerText = "Campo preenchido corretamente!";
+        botaoDisable.disabled = false;
     }
-
+// validação do telefone
     if (!telefone.value) {
-        errorTelefone.style.color = "white";
+        errorTelefone.style.color = "red";
         errorTelefone.innerText = "Insira seu Telefone para avançar" ; 
+        botaoDisable.disabled = true;
     
     }
     else if (telefone.value.length < 11) {
         errorTelefone.style.color = "orange";
-        errorTelefone.innerText = "Insira um telefone válido para avançar" ; 
+        errorTelefone.innerText = "Insira um telefone válido para avançar" ;
+        botaoDisable.disabled = true;
     }
     else {
         errorTelefone.style.color = "lime";
         errorTelefone.innerText = "Campo preenchido corretamente!" ; 
+        botaoDisable.disabled = false;
     }
-
+// validação do e-mail
     if (!email) {
-        errorE.style.color = "white";
+        errorE.style.color = "red";
         errorE.innerText = "Preencha com o email para avançar" ;
+        botaoDisable.disabled = true;
     
     }
     else if(!email.match(validade)) {
         errorE.style.color = "orange";
         errorE.innerText = "Insira um E-mail válido" ;
+        botaoDisable.disabled = true;
      }
     else {     
         errorE.style.color = "lime";
         errorE.innerText = "Campo preenchido corretamente!" ;
+        botaoDisable.disabled = false;
     }
-
+// validação da senha
     if (!senha.value) {
-        errorSenha.style.color = "white";
+        errorSenha.style.color = "red";
         errorSenha.innerText = "Insira a sua nova senha para avançar" ;
+        botaoDisable.disabled = true;
     }
     else if (senha.value.length < 8) {
         errorSenha.style.color = "orange";
         errorSenha.innerText = "Insira no mínimo 8 caracteres" ;
+        botaoDisable.disabled = true;
     }
     else {
         errorSenha.style.color = "lime";
         errorSenha.innerText = "Campo Preenchido corretamente" ;
+        botaoDisable.disabled = false;
     }
+}
+// resetar a página depois que concluir
+let button = document.querySelector("button")
+function cadastrar() {
+    button.addEventListener("click", () => {
+        window.location.reload();
+    });
 }
